38380487851b3542a41a9e4a9bd3aaf6ff6ad97e
Hiram Silvey <blujay270@gmail.com>
2023-07-22 18:45:45 -0400

    Added projectile base class. May need to move some of the initial values out of it later depending on how the projectiles diverge in implementation.

--- Source/ActionRoguelike/Public/SGameplayInterface.h
+++ Source/ActionRoguelike/Public/SGameplayInterface.h
@@ -20,7 +20,6 @@ class ACTIONROGUELIKE_API ISGameplayInterface
 {
 	GENERATED_BODY()
 
-	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
 	void Interact(APawn* InstigatorPawn);
