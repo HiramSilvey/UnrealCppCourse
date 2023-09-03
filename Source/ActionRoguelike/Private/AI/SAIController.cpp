8e06d5664aacfca95852711c48a88749731958bc
Hiram Silvey <blujay270@gmail.com>
2023-09-03 13:25:46 -0400

    Removed commented out code from previous lecture.

--- Source/ActionRoguelike/Private/AI/SAIController.cpp
+++ Source/ActionRoguelike/Private/AI/SAIController.cpp
@@ -13,11 +13,4 @@ void ASAIController::BeginPlay()
 	{
 		RunBehaviorTree(BehaviorTree);
 	}
-
-	/* APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0);
-	if (IsValid(MyPawn))
-	{
-		GetBlackboardComponent()->SetValueAsVector("MoveToLocation", MyPawn->GetActorLocation());
-		GetBlackboardComponent()->SetValueAsObject("TargetActor", MyPawn);
-	} */
 }
